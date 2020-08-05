3306b5ea2bde9327b0df0e2b3f02bcd1b26baaaa
manuelsanchez2 <manusansan22@gmail.com>
2020-08-05 12:20:24 +0200

    Install webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import { createElement } from "./src/utils/elements.js"
-import { createToggleButton } from "./src/components/toggleButton.js";
-import { createTitle } from "./src/components/title.js";
+import { createToggleButton } from "./components/toggleButton.js";
+import { createTitle } from "./components/title.js";
 
 const title = createTitle();
 const toggleButton = createToggleButton();
